722bc0aee6db1c9f2b1dcaf4d50c5ae939aa6ea7
Brandon Schmidt <brandonschmidt@Jeff-Myerss-iMac.local>
2015-05-05 12:52:51 -0400

    Adding a base zsh config for all times.

---
+++ zsh/.zsh/aliases.zsh
@@ -0,0 +1,11 @@
+# Colorize output, add file type indicator, and put sizes in human readable format
+alias ls='ls -GFh'
+
+# Same as above, but in long listing format
+alias ll='ls -GFhl'
+
+alias ri='ri -Tf ansi'
+
+alias mysql='mysql -u root'
+alias mysqladmin='mysqladmin -u root'
+
---
+++ zsh/README.md
@@ -0,0 +1,8 @@
+# ZSH
+
+This is a modified for my needs version of :
+
+https://github.com/spicycode/ze-best-zsh-config
+
+
+
